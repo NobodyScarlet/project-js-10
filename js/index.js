61bdc5105e3a4e84580941c6e172b07a944214e4
Yevhenii <89587950+NobodyScarlet@users.noreply.github.com>
2022-04-05 17:50:03 +0300

    up

---
+++ js/index.js
@@ -0,0 +1,40 @@
+'use strict';
+
+//  1
+const a = +prompt('Введите число');
+console.log(a === 0 ? console.log('верно') : console.log('не верно'));
+
+// 2
+
+const a = +prompt('Введите число');
+console.log(a > 0 ? console.log('верно') : console.log('не верно'));
+
+// 3
+
+const a = prompt('Введите число или букву');
+console.log(a < 0 ? console.log('верно') : console.log('не верно'));
+
+// 4
+
+const a = prompt('Введите а');
+console.log(a >= 0 ? console.log('верно') : console.log('не верно'));
+
+// 5
+
+const a = prompt('Введите а');
+console.log(a <= 0 ? console.log('верно') : console.log('не верно'));
+
+// 6
+
+const a = prompt('Введите а');
+console.log(a != 0 ? console.log('верно') : console.log('не верно'));
+
+// 7
+
+const a = prompt('Введите test');
+console.log(a === 'test' ? console.log('верно') : console.log('не верно'));
+
+//8
+
+const a = +prompt('Введите число');
+console.log(a == 0 ? console.log('верно') : console.log('не верно'));
